cce52ea0b9bdd3405b275334c73540e4848e0c69
Ritchie Frodomar <alkalinethunder@gmail.com>
2024-03-15 17:45:13 -0400

    Add support for actual social links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [
-    mdx(),
-    sitemap(), 
-    tailwind({
-      applyBaseStyles: false
-    })
-  ]
+  integrations: [mdx(), sitemap(), tailwind({
+    applyBaseStyles: false
+  }), icon()]
 });
--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,32 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const SOCIAL_LINKS = [
+    {
+        name: 'Mastodon',
+        link: 'https://mastodon.social/acidiclight',
+        icon: 'mdi:mastodon'
+    },
+    {
+        name: 'GitHub',
+        link: 'https://github.com/acidiclight',
+        icon: 'mdi:github'
+    },
+    {
+        name: 'Discord',
+        link: 'https://discord.gg/5qZUeeVvnT',
+        icon: 'mdi:discord'
+    },
+    {
+        name: 'Patreon',
+        link: 'https://patreon.com/acidiclight',
+        icon: 'mdi:patreon'
+    },
+    {
+        name: 'YouTube',
+        link: 'https://youtube.com/c/AcidicLight',
+        icon: 'mdi:youtube'
+    },
+    
+]
