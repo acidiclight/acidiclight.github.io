fbaeab19c59ba4e8a00d75aa9285953c5db69180
Ritchie Frodomar <alkalinethunder@gmail.com>
2024-03-15 00:22:10 -0400

    Revert "Set up Tailwind"
    This reverts commit d89f1c4e4adcef0d74392107d8ee15c46a21283f.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'acidic light';
-export const SITE_DESCRIPTION = 'Blind game developer and programmer most well-known for being lead programmer of Restitched!';
+export const SITE_TITLE = 'Astro Blog';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
